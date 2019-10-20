ef285fb59562f73bfd8d1cd95861d788386727cf
davidmihai98 <davidmihai98@yahoo.com>
2019-10-20 21:13:15 +0300

    main.c ready

--- main.c
+++ main.c
@@ -12,10 +12,33 @@ int main(int argc, char *argv[]) {
 		f = fopen(argv[1], "rb");
 
 		char ch;
-
+		int stare = 3;
 		while (!feof(f)) {
 			ch = fgetc(f);
-			parse(ch);
+			stare = parse(ch);
+			if (stare <= 0) {
+				printf("Eroare la starea %d", abs(stare));
+				break;
+			}
+		}
+		close(f);
+		
+		if (stare == 1) {
+			printf("\r\n");
+			if (at.line_count == 1) {
+				printf("%s\r\n", at.str[0]);
+			}
+			else {
+				int i;
+				for (i = 0; i < at.line_count; i++) {
+					printf("+%s\r\n", at.str[i]);
+				}
+				printf("\r\n");
+			}
+			printf("\r\n");
+		}
+		else {
+			printf("Mai este nevoie de caractere !");
 		}
 	}
 
