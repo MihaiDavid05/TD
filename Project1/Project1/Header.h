bfbc0b4a9df5f34ba2d2f4234a80fd1f8251c5dc
davidmihai98 <davidmihai98@yahoo.com>
2019-10-21 18:47:06 +0300

    AT command state machine

---
+++ Project1/Project1/Header.h
@@ -0,0 +1,16 @@
+#ifndef AUTOMAT_H
+#define AUTOMAT_H
+#include<stdint.h>
+#include<stdbool.h>
+#include<stdio.h>
+#define MAX_LINE 1000
+#define STR_SIZE 150
+typedef struct {
+	bool ok;
+	uint32_t line_count;
+	uint8_t str[MAX_LINE][STR_SIZE + 1];
+}AT_DATA;
+
+extern AT_DATA at;
+int parser(char);
+#endif // !AUTOMAT_H
---
+++ Project1/Project1/Source.c
@@ -0,0 +1,273 @@
+#include "Header.h"
+AT_DATA at;
+
+int parser(char ch)
+{
+	static uint8_t state = 0;
+	static uint32_t line_index, string_index;
+	switch (state)
+	{
+	case 0:
+
+		if (ch == 0x0D)
+		{
+			state = 1;
+		}
+		break;
+
+	case 1:
+		if (ch == 0x0A)
+		{
+			state = 2;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 2:
+		if (ch == 'E')
+		{
+			state = 3;
+		}
+		else {
+			if (ch == 'O')
+			{
+				state = 10;
+			}
+			else {
+				if (ch == '+')
+				{
+					state = 11;
+				}
+				else {
+					line_index = 0;
+					string_index = 0;
+					return -state;
+				}
+			}
+		}
+		break;
+	case 3:
+		if (ch == 'R')
+		{
+			state = 4;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 4:
+		if (ch == 'R')
+		{
+			state = 5;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 5:
+		if (ch == 'O')
+		{
+			state = 6;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 6:
+		if (ch == 'R')
+		{
+			state = 7;
+			at.ok = false;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 7:
+		if (ch == 0x0D)
+		{
+			state = 8;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 8:
+		if (ch == 0x0A)
+		{
+			state = 9;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 9:
+	{
+		if (at.ok == false) {
+			return 1;
+		}
+		else {
+			return 1;
+		}
+	}
+	case 10:
+		if (ch == 'K')
+		{
+			state = 7;
+			at.ok = true;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 11:
+		if (ch != 0x0D)
+		{
+			state = 11;
+			at.str[line_index][string_index++] = ch;
+		}
+		else
+		{
+			if (ch == 0x0D)
+			{
+				at.str[line_index++][string_index] = '\0';
+				string_index = 0;
+				at.line_count++;
+				state = 12;
+			}
+			else {
+				line_index = 0;
+				string_index = 0;
+				return -state;
+			}
+		}
+		break;
+	case 12:
+		if (ch == 0x0A)
+		{
+			state = 13;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 13:
+		if (ch == '+')
+		{
+			state = 11;
+		}
+		else
+		{
+			if (ch == 0x0D)
+			{
+				state = 14;
+			}
+			else {
+				line_index = 0;
+				string_index = 0;
+				return -state;
+			}
+		}
+		break;
+	case 14:
+		if (ch == 0x0A)
+		{
+			state = 15;
+		}
+		break;
+	case 15:
+		if (ch == 'O')
+		{
+			state = 16;
+		}
+		else {
+			if (ch == 'E')
+			{
+				state = 17;
+			}
+			else {
+				line_index = 0;
+				string_index = 0;
+				return -state;
+			}
+		}
+		break;
+	case 16:
+		if (ch == 'K')
+		{
+			state = 7;
+			at.ok = true;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 17:
+		if (ch == 'R')
+		{
+			state = 18;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 18:
+		if (ch == 'R')
+		{
+			state = 19;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 19:
+		if (ch == 'O')
+		{
+			state = 20;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	case 20:
+		if (ch == 'R')
+		{
+			state = 7;
+			at.ok = false;
+		}
+		else {
+			line_index = 0;
+			string_index = 0;
+			return -state;
+		}
+		break;
+	}
+	return 2;
+}
